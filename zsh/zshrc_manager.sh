565664a542f577e36ee1de0f1eca225bda32baf9
justinethomas009 <justinethomas009@gmail.com>
2018-04-20 19:56:50 +0530

    offline support

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,4 +1,4 @@
-time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
+#time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 # Run tmux if exists
 if command -v tmux>/dev/null; then
@@ -7,7 +7,17 @@ else
 	echo "tmux not installed. Run ./deploy to configure dependencies"
 fi
 
-echo "Updating configuration"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
-(cd ~/dotfiles && git pull && git submodule update --init --recursive)
+
+timeout 2 ping -q -c1 google.com > /dev/null
+ 
+if [ $? -eq 0 ]
+then
+    echo "online";
+    echo "Updating configuration"
+    (cd ~/dotfiles && git pull && git submodule update --init --recursive)
+else
+    echo "offline";
+fi
+
 source ~/dotfiles/zsh/zshrc.sh
